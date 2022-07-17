aa480bec2b9efce68b7c98427341c711cbad0851
NikarashiHatsu <yourlovelydev@gmail.com>
2022-07-18 03:08:08 +0700

    chore: separate libraries

---
+++ resources/js/swiper.js
@@ -0,0 +1,8 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/effect-cards";
+
+window.Swiper = Swiper;
+window.Swiper.Navigation = Navigation;
+window.Swiper.Pagination = Pagination;
