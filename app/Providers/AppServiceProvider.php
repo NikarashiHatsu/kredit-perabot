00938431199b21b5408b5cedd35031b74a1c302c
NikarashiHatsu <yourlovelydev@gmail.com>
2022-07-18 17:26:55 +0700

    chore: add faker provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        fake()->addProvider(new \Bezhanov\Faker\Provider\Commerce(fake()));
     }
 }
