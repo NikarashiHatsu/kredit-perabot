0c9fbffe514ffbf8d8cf6302c5f4822302ef0c6a
NikarashiHatsu <yourlovelydev@gmail.com>
2022-07-21 01:06:14 +0700

    i forgot to add subcategory feature ☠️

--- resources/views/dashboard/subcategory/edit.blade.php
+++ resources/views/dashboard/subcategory/edit.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="flex items-center justify-between mb-4">
-        <a href="{{ route('dashboard.category.index') }}" class="transition duration-300 ease-in-out bg-blue-500 text-white rounded px-4 py-2">
+        <a href="{{ route('dashboard.subcategory.index') }}" class="transition duration-300 ease-in-out bg-blue-500 text-white rounded px-4 py-2">
             Kembali
         </a>
         <h5 class="text-xl font-bold">
@@ -11,14 +11,29 @@
     <x-alerts/>
 
     <div class="bg-white p-6 rounded border border-gray-300">
-        <form action="{{ route('dashboard.category.update', $category) }}" method="post">
+        <form action="{{ route('dashboard.subcategory.update', $subcategory) }}" method="post">
             @csrf
             @method('PUT')
             <div class="grid grid-cols-12 grid-flow-row gap-4">
                 <div class="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3">
                     <div class="flex flex-col">
+                        <label for="category_id" class="mb-2">Asal Subkategori</label>
+                        <select name="category_id" id="category_id" class="border border-gray-300 rounded text-sm">
+                            @foreach ($categories as $id => $name)
+                                <option {{ $subcategory->category_id == $id ? "selected" : "" }} value="{{ $id }}">{{ $name }}</option>
+                            @endforeach
+                        </select>
+                        @error('category_id')
+                            <p class="text-xs text-red-500 mt-2 italic">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3">
+                    <div class="flex flex-col">
                         <label for="name" class="mb-2">Nama Kategori</label>
-                        <input type="text" name="name" id="name" class="border border-gray-300 rounded text-sm" value="{{ $category->name }}" required autofocus />
+                        <input type="text" name="name" id="name" class="border border-gray-300 rounded text-sm" value="{{ $subcategory->name }}" required autofocus />
                         @error('name')
                             <p class="text-xs text-red-500 mt-2 italic">
                                 {{ $message }}
